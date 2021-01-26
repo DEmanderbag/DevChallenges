c55ed534b2ea502911c644e230a94e8187cb4ad5
DEmanderbag <dusan-milosevic@outlook.com>
2021-01-26 22:56:19 +0100

    Add ability to see up to 10 quotes from Author

--- quoteGenerator/app.js
+++ quoteGenerator/app.js
@@ -1,19 +1,87 @@
+const body = document.querySelector("body");
 const ctaBtn = document.querySelector(".random__quote");
+const quoteSection = document.querySelector(".quote");
+const allQuotesSection = document.querySelector(".all__quotes");
 
 ctaBtn.addEventListener("click", getRandomQuote);
 
+const API = "https://quote-garden.herokuapp.com/api/v3/quotes";
+let params;
+
 async function getRandomQuote() {
-  const request = await fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random");
+  // Returns a random quote
+  const request = await fetch(`${API}/random`);
   const data = await request.json();
 
-  const quoteSection = document.querySelector(".quote");
+  // Save data in variables
+  let quoteText = data.data[0].quoteText;
+  let quoteAuthor = data.data[0].quoteAuthor;
+  let quoteGenre = data.data[0].quoteGenre;
+
   let quote = `
-  <p class="quote__text">${data.data[0].quoteText}</p>
+  <p class="quote__text">${quoteText}</p>
     <div class="quote__about">
-      <p class="quote__author">${data.data[0].quoteAuthor}</p>
-      <p class="quote__genre">${data.data[0].quoteGenre}</p>
+      <p class="quote__author">${quoteAuthor}</p>
+      <p class="quote__genre">${quoteGenre}</p>
     </div>`;
   quoteSection.innerHTML = quote;
+
+  //  Get the data from Parametar
+  params = new URLSearchParams({
+    author: quoteAuthor,
+    limit: 10,
+  });
+
+  const quoteAbout = document.querySelector(".quote__about");
+  quoteAbout.addEventListener("click", () => {
+    allQuotesSection.classList.add("is-open");
+    clearAllQuotes();
+    body.classList.add("open");
+    getQuoteByAuthor();
+  });
+}
+
+// Clear all quotes window
+function clearAllQuotes() {
+  allQuotesSection.innerHTML = "";
+}
+
+async function getQuoteByAuthor() {
+  const url = await fetch(`${API}?${params.toString()}`);
+  const authorQuote = await url.json();
+  let allQuotes = authorQuote.data;
+
+  let authorName = allQuotes[0].quoteAuthor;
+  console.log(authorName);
+
+  let backButton = `
+  <div class="back__group">
+    <button class="back__option">
+      <img src="assets/icons/back.svg" alt="arrow to the left">
+      <p class="quote__author quote__author--all">${authorName}</p>
+    </button>
+   </div>`;
+
+  allQuotesSection.innerHTML += backButton;
+
+  allQuotes.forEach((e) => {
+    let quotes = e.quoteText;
+    let quotesData = `
+      <div class="test">
+        <p>${quotes}</p>
+      </div>`;
+
+    console.log(quotes);
+    allQuotesSection.innerHTML += quotesData;
+  });
+
+  let goBack = document.querySelector(".back__option");
+
+  goBack.addEventListener("click", () => {
+    allQuotesSection.classList.remove("is-open");
+    body.classList.remove("open");
+  });
+  console.log("loaded");
 }
 
 getRandomQuote();
